32228291ff8a7d85565dfd87b605abdc86cc9c60
Sohail Afzal <sohail.afzal@barqaab.com>
2023-03-12 11:41:01 +0500

    12-03-2023

--- lib/utils/api/BaseAPI.dart
+++ lib/utils/api/BaseAPI.dart
@@ -1,6 +1,7 @@
 class BaseAPI {
-  static final String baseURL = "https://www.barqaab.pk/hrms/public/api/";
-  //"http://localhost/hrms/public/api/";
+  static final String baseURL = "http://localhost/hrms/public/api/";
+  //"https://www.barqaab.pk/hrms/public/api/";
+  //
   //
 }
 
